e513a8bdbc2193e25df5b124d4f90e95514af5a1
Amin Tazifor <amin.tazifor@gmail.com>
2024-08-30 12:10:16 +0100

    add configuration to manage beans
    make application context instance aware of the configuration

---
+++ src/main/java/config/ProjectConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import main.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+    @Bean //during context initialization, call this method and add  returned value  to the context
+    Parrot parrot() { //method name becomes bean's name
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        //instruct application context instance to use configuration class
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        //parrot is now part of context
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println("p.getName() = " + p.getName());
+    }
+}
