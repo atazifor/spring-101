a66fc49ecbeb1ad55d841c9ede087037865b70ab
Amin Tazifor <amin.tazifor@gmail.com>
2024-08-30 11:53:18 +0100

    create a spring context
    context not yet aware of other objects

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        //create a new instance of spring context
+        ApplicationContext context = new AnnotationConfigApplicationContext();
         Parrot parrot  = new Parrot();
     }
 }
